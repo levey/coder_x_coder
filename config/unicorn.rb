450720773dade4942519e29cfbaeafd11bc62c5d
levey <levey.zhu@gmail.com>
2012-03-28 18:05:29 +0800

    updated unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -40,7 +40,7 @@ before_fork do |server, worker|
 
   # Before forking, kill the master process that belongs to the .oldbin PID.
   # This enables 0 downtime deploys.
-  old_pid = "/tmp/unicorn.coder_x_coder.pid.oldbin"
+  old_pid = "#{shared_path}/tmp/unicorn.pid.oldbin"
   if File.exists?(old_pid) && server.pid != old_pid
     begin
       Process.kill("QUIT", File.read(old_pid).to_i)
