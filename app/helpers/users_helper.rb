ab9006734acd7380b78bcbcc2b24ea3cdd18afae
levey <levey.zhu@gmail.com>
2012-04-01 18:32:52 +0800

    updated css file

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,9 +2,9 @@ module UsersHelper
   
   def gravatar_for(user, options = { :size => 50, :round => false })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png?size=#{options[:size]}"
-    avatar_class = options[:round] ? "gravatar-header" : "gravatar"
-    image_tag(gravatar_url, alt: user.username, class: avatar_class)  
+    size = options[:size]
+    gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png?size=#{size}"
+    image_tag(gravatar_url, alt: user.username, width:"#{size}", height:"#{size}")  
   end
   
   def current_user?(user)
