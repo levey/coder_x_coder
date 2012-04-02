8331feb063a5c6442bdf98c5e3ea3ae8188d1749
levey <levey.zhu@gmail.com>
2012-04-02 16:01:02 +0800

    improved firefox compatibility

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,18 @@
 module UsersHelper
   
-  def gravatar_for(user, options = { :size => 50, :round => false })
+  def gravatar_for(user, options = { :size => 50 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
     gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png?size=#{size}"
-    image_tag(gravatar_url, alt: user.username, width:"#{size}", height:"#{size}")  
+    image_tag(gravatar_url, alt: user.username, width:"#{size}px", height:"#{size}px")  
   end
+
+  def header_gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png?size=20"
+    image_tag(gravatar_url, alt: user.username, class: "gravatar-header")  
+  end
+  
   
   def current_user?(user)
     user == current_user
