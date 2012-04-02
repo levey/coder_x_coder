099524df05f8f16f7b6d6ba46a238bcce5d4b078
levey <levey.zhu@gmail.com>
2012-04-02 18:14:25 +0800

    added default avatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
   
-  def gravatar_for(user, options = { :size => 50 })
+  def gravatar_for(user, options = { :size => 48 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
     gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png?size=#{size}"
