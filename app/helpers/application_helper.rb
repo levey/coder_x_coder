5b663b0f2bfcf723a4d079d9cd7f300feca24cfe
levey <levey.zhu@gmail.com>
2012-04-10 17:52:02 +0800

    completed notification

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,17 @@ module ApplicationHelper
     end
   end
 
+  def process_mentions(text)
+    text.gsub(Comment::MENTION_REGEXP) do
+      if $1.present? && !User.find_by_username($1).nil?
+        %(<a href="/users/#{User.find_by_username($1).id}">@#{$1}</a>)
+      else
+        "@#{$1}"
+      end
+    end
+  end
+
   def generate_markdown(text)
-    markdown = RDiscount.new(text, :smart).to_html.html_safe
+    markdown = RDiscount.new(process_mentions(text), :smart).to_html.html_safe
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ActiveRecord::Base
+  MENTION_REGEXP = /@(\p{Word}+)/u
+  
   belongs_to :user
   belongs_to :topic, :counter_cache => true
   attr_protected :user_id, :topic_id
@@ -10,6 +12,17 @@ class Comment < ActiveRecord::Base
   after_save :send_notifications
   after_save :update_topic
   
+  def has_mentions?
+    self.content =~ MENTION_REGEXP
+  end
+  
+  def mentioned_users
+    mentioned_names = self.content.scan(MENTION_REGEXP).collect {|matched| matched.first}.uniq
+    mentioned_names.delete(self.user.username)
+    mentioned_names.delete(self.topic.user.username)
+    mentioned_names.map { |name| User.find_by_username(name) }.compact
+  end
+  
   private
   
     def update_topic
@@ -18,8 +31,19 @@ class Comment < ActiveRecord::Base
     
     def send_notifications
       unless self.user == self.topic.user
-        Notification.save_notification(self.user, self.topic.user, self.topic, self.content)      
+        send_notification_to(self.topic.user, self.topic)     
+      end
+      send_notification_to_mentioned_users if self.has_mentions?
+    end
+    
+    def send_notification_to_mentioned_users
+      mentioned_users.each do |receiver|
+        send_notification_to(receiver, self)
       end
     end
-  
+    
+    def send_notification_to(receiver, notifiable)
+      Notification.save_notification(self.user, receiver, notifiable, self.content)
+    end
+      
 end
